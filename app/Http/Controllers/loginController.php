8a1a6eadedf2f9cde5e35613b9da7d742906aa19
amine sakhri <sakhriamine2003@gmail.com>
2026-07-05 18:56:03 +0100

    creation un user qui ajouter

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Auth;
 
 class loginController extends Controller
 {
+
+
     public function show()
     {
         return view('login.show');
@@ -19,60 +21,39 @@ class loginController extends Controller
         return view('login.register');
     }
 
-    public function register(Request $request)
-    {
-        // 1. validation
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8',
-        ]);
 
-        // 2. créer user f database
+    
+    public function register(Request $r)
+    {
+    
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        // 3. connecter user automatiquement
-        Auth::login($user);
-
-        // 4. redirect
-        return redirect('/feed');
-    }
-
-    public function login(Request $request)
-    {
-        // 1. validation
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        // 2. vérifier email + password
-        if (Auth::attempt($request->only('email', 'password'))) {
-
-            // حماية session
-            $request->session()->regenerate();
-
-            // ila login صحيح
-            return redirect('/feed');
-        }
-
-        // ila email ola password ghalat
-        return back()->withErrors([
-            'email' => 'Email ou mot de passe incorrect.',
-        ])->onlyInput('email');
-    }
-
-    public function logout(Request $request)
-    {
-        Auth::logout();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+            'name' => $r->name,
+            'email' => $r->email,
+            'password' => Hash::make($r->password),
+        ]);        
 
         return redirect('/login');
     }
-}
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+        
+//         return back()->withErrors([
+//             'email' => 'Email ou mot de passe incorrect.',
+//         ]);
+//     }
+ }
+
